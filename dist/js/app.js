950cbcb90c01d8f3158583a61a71c01972fabf02
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-15 13:22:46 +0200

    Add meet the team section with cards

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,21 +12,41 @@ var app = new Vue({
   data: {
     // Nav links
     links: [{
-      name: 'Home'
+      name: 'Home',
+      link: '#site_header'
     }, {
-      name: 'Who We Are'
+      name: 'Who We Are',
+      link: '#meet'
     }, {
-      name: 'What We Do'
+      name: 'What We Do',
+      link: '#services'
     }, {
-      name: 'Where We Work'
+      name: 'Where We Work',
+      link: '#work'
     }, {
       name: 'Careers',
+      link: '#plans',
       "class": 'apply'
     }, {
-      name: 'News'
+      name: 'News',
+      link: '#news'
     }, {
       name: 'GET IN TOUCH NOW',
+      link: '#contact',
       "class": 'btn_y'
+    }],
+    cards: [{
+      icon: 'fas fa-road',
+      title: 'Establish Goals',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad accusamus id delectus cumque eaque atque odit praesentium explicabo obcaecati rem?'
+    }, {
+      icon: 'fas fa-video',
+      title: 'Work With A Team',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora accusantium aut culpa quo, libero beatae earum architecto sapiente at. Eveniet!'
+    }, {
+      icon: 'fas fa-tint',
+      title: 'Get Results',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odit, nesciunt repellat officiis praesentium esse voluptatum ex optio nostrum earum.'
     }]
   },
   methods: {}
--- src/app.js
+++ src/app.js
@@ -4,13 +4,18 @@ const app = new Vue({
     data: {
         // Nav links
         links: [
-            {name: 'Home'},
-            {name: 'Who We Are'},
-            {name: 'What We Do'},
-            {name: 'Where We Work'},
-            {name: 'Careers', class: 'apply'},
-            {name: 'News'},
-            {name: 'GET IN TOUCH NOW', class: 'btn_y'},
+            {name: 'Home', link:'#site_header'},
+            {name: 'Who We Are', link: '#meet'},
+            {name: 'What We Do', link: '#services'},
+            {name: 'Where We Work', link: '#work'},
+            {name: 'Careers',link:'#plans', class: 'apply'},
+            {name: 'News', link:'#news'},
+            {name: 'GET IN TOUCH NOW',link:'#contact', class: 'btn_y'},
+        ],
+        cards: [
+            {icon: 'fas fa-road', title: 'Establish Goals', paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad accusamus id delectus cumque eaque atque odit praesentium explicabo obcaecati rem?'},
+            {icon: 'fas fa-video', title: 'Work With A Team', paragraph: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora accusantium aut culpa quo, libero beatae earum architecto sapiente at. Eveniet!'},
+            {icon: 'fas fa-tint', title: 'Get Results', paragraph:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odit, nesciunt repellat officiis praesentium esse voluptatum ex optio nostrum earum.'}
         ]
     },
 
